e109d541294437aa82dec97cf50f5ac3a9ccef38
gaoyi <31531997@qq.com>
2024-07-21 12:19:44 +0800

    有序表的合并——用链表实现

--- HelloWorld_p1/signalLinklist.cpp
+++ HelloWorld_p1/signalLinklist.cpp
@@ -251,6 +251,7 @@ int union_list(LinkList& La, LinkList Lb) {
 
 /*
 * 有序表的合并——用链表实现
+* 时间复杂度：O(length(La) + length(Lb))，最差的情况下，La和Lb交替插入
 */
 int union_list_orderd(LList& La, LList& Lb,LList &Lc) {
 
