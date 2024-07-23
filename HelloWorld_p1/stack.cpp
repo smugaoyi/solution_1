8231a2d5a1f886c0d3ac7f3f7c1dba6b7a3ff1af
gaoyi <31531997@qq.com>
2024-07-23 10:08:48 +0800

    后进先出

--- HelloWorld_p1/stack.cpp
+++ HelloWorld_p1/stack.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // Last In First Out 的线性表，简称LIFO结构。
+// 顺序栈只能在数组的尾部进行操作，链栈只能在链表的头部进行操作。
 
 #define MAXSIZE 10
 typedef struct {
@@ -24,6 +25,32 @@ int initStack(SqStack& S) {
 	}
 	S.top = S.base;
 	S.stacksize = MAXSIZE;
+
+	return 0;
+}
+
+/*
+* 顺序栈的入栈
+*/
+int push(SqStack& S, SElemType e) {
+	if (S.top - S.base == S.stacksize) {
+		return -1;
+	}
+	*S.top = e;
+	S.top++;
+
+	return 0;
+}
+
+/*
+* 顺序栈的出栈
+*/
+int pop(SqStack& S, SElemType& e) {
+	if (S.top == S.base) {
+		return -1;
+	}
+	S.top--;
+	e = *S.top;
 
 	return 0;
 }
