0866be15305cfc5c16a9bd7b2cb0a15cc2ce7bf3
gaoyi <31531997@qq.com>
2024-08-05 13:29:42 +0800

    串

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Person
+{
+public:
+	Person();
+	~Person();
+
+private:
+	int age;
+	int high;
+
+};
+
+Person::Person()
+{
+	cout << "构造函数调用" << endl;
+}
+
+Person::~Person()
+{
+	cout << "析构函数调用" << endl;
+}
+
+
+
+
+int main()
+{
+	std::cout << "Hello World!\n";
+
+	Person p;
+}
---
+++ HelloWorld_p1/7_sequence_string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+// ×Ö·û´®µÄ Ë³Ðò´æ´¢
+
+#define MAXLEN 255
+typedef struct {
+	char ch[MAXLEN + 1];
+	int length;
+}SString;
